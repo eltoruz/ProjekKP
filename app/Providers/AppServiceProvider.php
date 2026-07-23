57e9fd73e74ca902a6ef5626b94ed80bf671c96a
MuhammadBintangAl-Fasya <bintangalfasya32@gmail.com>
2026-07-23 08:22:56 +0700

    refactor: remove unused dependencies, filament remnants, dead code, and temporary files

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,14 +13,6 @@ class AppServiceProvider extends ServiceProvider
 
     public function boot(): void
     {
-        $this->app->bind(
-            \Filament\Http\Responses\Auth\Contracts\LogoutResponse::class,
-            fn () => new class implements \Filament\Http\Responses\Auth\Contracts\LogoutResponse {
-                public function toResponse($request): \Illuminate\Http\RedirectResponse
-                {
-                    return redirect('/');
-                }
-            }
-        );
+        //
     }
 }
